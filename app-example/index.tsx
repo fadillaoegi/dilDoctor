bd22d3005c56726da070dc1c0f53ba9e93320821
Fadil <fadilfdl2710@gmail.com>
2024-06-30 08:46:40 +0700

    save

---
+++ app-example/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const MainApp = () => {
+  return (
+    <view>
+      <Text>Test</Text>
+    </view>
+  );
+};
+
+export default MainApp;
---
+++ app-example/screens/splash/index.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+const Splash = () => {
+  return <View></View>;
+};
+
+export default Splash;
